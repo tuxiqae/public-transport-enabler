b378564ddc0ed51141b08c71faabc4a3c71bd5c3
felix <felix@delattre.de>
2017-12-04 23:33:15 +0100

    Revert "Merge commit 'refs/pull/99/head' of github.com:schildbach/public-transport-enabler"
    This reverts commit bc7816ebc367e9cd41f95d3f4b785210d659dc3a, reversing
    changes made to 971b1230270465d8d435efc426f6cc515ad60198.

--- enabler/src/de/schildbach/pte/NetworkId.java
+++ enabler/src/de/schildbach/pte/NetworkId.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 the original author or authors.
+ * Copyright 2010-2015 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -73,7 +73,7 @@ public enum NetworkId {
     DUB,
 
     // United States
-    RTACHICAGO, OREGON, TEXAS,
+    RTACHICAGO, OREGON,
 
     // Canada
     ONTARIO, QUEBEC,
